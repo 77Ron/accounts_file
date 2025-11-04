aff1bf2195a192477ccbf318aa97eda254dd5e10
Ron <ron@wymanfineart.co.uk>
2025-11-04 07:43:01 +0000

    Addtions to update screen

--- accounts_desktop.py
+++ accounts_desktop.py
@@ -7,13 +7,25 @@ from tkinter import ttk
 from tkinter.ttk import *
 from company_info import *
 
-def save(name_var):
-    name=name_var.get()  
+def save(name_v):
+    name=name_v.get()  
     print("Username is: " + name)
-    name_var.set("")
+    name_v.set("")
 
-def dept(dp1):
-    dept=dp1 
+def dept(dp1, button1, button2, button3, estndchrg_v):
+    dept=dp1
+    button1.configure(bg='SteelBlue3')
+    button2.configure(bg=btcol)
+    button3.configure(bg=btcol)
+
+    if dept == 1:
+        stnd=stnrd_fee1
+    elif dept == 2:
+        stnd=stnrd_fee2
+    elif dept ==3:       
+        stnd=stnrd_fee3
+
+    estndchrg_v.set(stnd)
 
 def UpdateAccounts():
     root = tk.Tk() 
@@ -24,43 +36,49 @@ def UpdateAccounts():
                     foreground="gray25", background="white smoke") 
     label.pack()
 
-    frame = tk.Frame(root, width = '500', height = '700', bg=bgcol)
+    frame = tk.Frame(root, width = '550', height = '700', bg=bgcol)
     frame.pack()
 
     #-------------------
 
     balance_total = 0.00
 
-    name_var = tk.StringVar()
-    ename_var = tk.StringVar()
-    esname_var = tk.StringVar()
-    eid_var = tk.IntVar()
-    eaddress1_var = tk.StringVar()
-    eaddress2_var = tk.StringVar()
-    eaddress3_var = tk.StringVar()
-    ecode_var = tk.StringVar()
-    emobile_var = tk.StringVar()
-    eemail_var = tk.StringVar()
+    name_v = tk.StringVar()
+    ename_v = tk.StringVar()
+    esname_v = tk.StringVar()
+    eid_v = tk.IntVar()
+    eaddress1_v = tk.StringVar()
+    eaddress2_v = tk.StringVar()
+    eaddress3_v = tk.StringVar()
+    ecode_v = tk.StringVar()
+    emobile_v = tk.StringVar()
+    eemail_v = tk.StringVar()
+    etrc_v = tk.StringVar()
+
+    estndchrg_v = tk.DoubleVar()
+    eothrchrg_v = tk.DoubleVar()
+    epayment_v = tk.DoubleVar()
+    
 
     uname_label = tk.Label(root, text = 'Username:', font=('calibre',10, 'bold'), bg=bgcol)
-    uname_entry = tk.Entry(root,textvariable = name_var, font=('calibre',10,'normal'))
+    uname_entry = tk.Entry(root,textvariable = name_v, font=('calibre',10,'normal'))
     uname_label.place(x=10, y=60)
     uname_entry.place(x=90, y=60)
 
     #-------Customer-------
 
     eid_label = tk.Label(root, text = 'ID/Account No.:', font=('calibre',10, 'bold'), bg=bgcol)
-    eid_entry = tk.Entry(root,textvariable = eid_var, font=('calibre',10,'normal'))
+    eid_entry = tk.Entry(root,textvariable = eid_v, font=('calibre',10,'normal'))
     eid_label.place(x=222, y=105)
     eid_entry.place(x=330, y=105)
 
     ename_label = tk.Label(root, text = 'Customer Name:', font=('calibre',10, 'bold'), bg=bgcol)
-    ename_entry = tk.Entry(root,textvariable = ename_var, font=('calibre',10,'normal'))
+    ename_entry = tk.Entry(root,textvariable = ename_v, font=('calibre',10,'normal'))
     ename_label.place(x=10, y=155)
     ename_entry.place(x=126, y=155)
 
     esname_label = tk.Label(root, text = 'Surname:', font=('calibre',10, 'bold'), bg=bgcol)
-    esname_entry = tk.Entry(root,textvariable = esname_var, font=('calibre',10,'normal'))
+    esname_entry = tk.Entry(root,textvariable = esname_v, font=('calibre',10,'normal'))
     esname_label.place(x=52, y=180)
     esname_entry.place(x=126, y=180)
 
@@ -69,50 +87,78 @@ def UpdateAccounts():
     #-------Address-------
 
     eaddress1_label = tk.Label(root, text = 'Address1:', font=('calibre',10, 'bold'), bg=bgcol)
-    eaddress1_entry = tk.Entry(root,textvariable = eaddress1_var, font=('calibre',10,'normal'), width=25)
+    eaddress1_entry = tk.Entry(root,textvariable = eaddress1_v, font=('calibre',10,'normal'), width=25)
     eaddress1_label.place(x=54, y=205)
     eaddress1_entry.place(x=126, y=205)
 
     eaddress2_label = tk.Label(root, text = 'Address2:', font=('calibre',10, 'bold'), bg=bgcol)
-    eaddress2_entry = tk.Entry(root,textvariable = eaddress2_var, font=('calibre',10,'normal'), width=25)
+    eaddress2_entry = tk.Entry(root,textvariable = eaddress2_v, font=('calibre',10,'normal'), width=25)
     eaddress2_label.place(x=53, y=230)
     eaddress2_entry.place(x=126, y=230)
 
     eaddress2_label = tk.Label(root, text = 'Address3:', font=('calibre',10, 'bold'), bg=bgcol)
-    eaddress2_entry = tk.Entry(root,textvariable = eaddress3_var, font=('calibre',10,'normal'), width=25)
+    eaddress2_entry = tk.Entry(root,textvariable = eaddress3_v, font=('calibre',10,'normal'), width=25)
     eaddress2_label.place(x=53, y=255)
     eaddress2_entry.place(x=126, y=255)
 
     ecode_label = tk.Label(root, text = 'Post Code:', font=('calibre',10, 'bold'), bg=bgcol)
-    ecode_entry = tk.Entry(root,textvariable = ecode_var, font=('calibre',10,'normal'), width=8)
+    ecode_entry = tk.Entry(root,textvariable = ecode_v, font=('calibre',10,'normal'), width=8)
     ecode_label.place(x=46, y=280)
     ecode_entry.place(x=126, y=280)
 
-    ecode_label = tk.Label(root, text = 'Mobile:', font=('calibre',10, 'bold'), bg=bgcol)
-    ecode_entry = tk.Entry(root,textvariable = emobile_var, font=('calibre',10,'normal'), width=10)
-    ecode_label.place(x=67, y=305)
-    ecode_entry.place(x=126, y=305)
+    emobile_label = tk.Label(root, text = 'Mobile:', font=('calibre',10, 'bold'), bg=bgcol)
+    emobile_entry = tk.Entry(root,textvariable = emobile_v, font=('calibre',10,'normal'), width=10)
+    emobile_label.place(x=67, y=305)
+    emobile_entry.place(x=126, y=305)
 
-    ecode_label = tk.Label(root, text = 'Email:', font=('calibre',10, 'bold'), bg=bgcol)
-    ecode_entry = tk.Entry(root,textvariable = eemail_var, font=('calibre',10,'normal'), width=30)
-    ecode_label.place(x=73, y=330)
-    ecode_entry.place(x=126, y=330)
+    eemail_label = tk.Label(root, text = 'Email:', font=('calibre',10, 'bold'), bg=bgcol)
+    eemail_entry = tk.Entry(root,textvariable = eemail_v, font=('calibre',10,'normal'), width=30)
+    eemail_label.place(x=73, y=330)
+    eemail_entry.place(x=126, y=330)
 
-    ecode_label = tk.Label(root, text = 'Balance: '+ str(balance_total), font=('garamond', 15, 'bold'), bg=bgcol)
-    ecode_label.place(x=360, y=350)
+    ebalance_label = tk.Label(root, text = 'Balance: '+ str(balance_total), font=('garamond', 15, 'bold'), bg=bgcol)
+    ebalance_label.place(x=360, y=350)
 
 
-    #----Department--------
-    dept_btn = tk.Button(root, text = '1 '+ dept1, command = dept(1), font=('garamond', 12, 'bold'), bg=btcol, fg='black')
-    dept_btn.place(x=25, y=370)
-    dept_btn = tk.Button(root, text = '2 ' +dept2, command = dept(2), font=('garamond', 12, 'bold'), bg=btcol, fg='black')
-    dept_btn.place(x=40+len(dept1)*10, y=370)
-    dept_btn = tk.Button(root, text = '3 '+dept3, command = dept(3), font=('garamond', 12, 'bold'), bg=btcol, fg='black')
-    dept_btn.place(x=50+len(dept1)*10+len(dept2)*10, y=370)
+    #----Transactions--------
+
+    dept1_btn = tk.Button(root, text = '1 '+dept1, 
+                          command = lambda: dept(1, dept1_btn, dept2_btn, dept3_btn, estndchrg_v), 
+                          font=('garamond', 12, 'bold'), bg=btcol, fg='black')
+    dept1_btn.place(x=25, y=370)
+    dept2_btn = tk.Button(root, text = '2 '+dept2,
+                          command = lambda: dept(2, dept2_btn, dept1_btn, dept3_btn, estndchrg_v), 
+                          font=('garamond', 12, 'bold'), bg=btcol, fg='black')
+    dept2_btn.place(x=40+len(dept1)*10, y=370)
+    dept3_btn = tk.Button(root, text = '3 '+dept3, 
+                          command = lambda: dept(3, dept3_btn, dept1_btn, dept2_btn, estndchrg_v), 
+                          font=('garamond', 12, 'bold'), bg=btcol, fg='black')
+    dept3_btn.place(x=50+len(dept1)*10+len(dept2)*10, y=370)
+
+    etr_label = tk.Label(root, text = 'Standard Charge'+' '*10+'Other Charges'+' '*25+'Payment' , font=('garamond', 13, 'bold'), bg=bgcol)
+    etr_label.place(x=20, y=410)
+
+
+ 
+    estndchrg_entry = tk.Entry(root,textvariable = estndchrg_v, font=('calibre',10,'normal'), width=10)
+    estndchrg_entry.place(x=21, y=440)
+
+    eothrchrg_entry = tk.Entry(root,textvariable = eothrchrg_v, font=('calibre',10,'normal'), width=10)
+    eothrchrg_entry.place(x=180, y=440)
+
+    epayment_entry = tk.Entry(root,textvariable = epayment_v, font=('calibre',10,'normal'), width=10)
+    epayment_entry.place(x=385, y=440)
+
+
+
+    etrc_label = tk.Label(root, text = 'Transaction Comments:', font=('calibre',10, 'bold'), bg=bgcol)
+    etrc_entry = tk.Entry(root,textvariable = etrc_v, font=('calibre',10,'normal'), width=75)
+    etrc_label.place(x=10, y=605)
+    etrc_entry.place(x=10, y=630)
 
     #---Save----
-    save_btn = tk.Button(root,text = 'Save Updates', command = lambda: save(name_var), font=('garamond', 12, 'bold'), bg=btcol, fg='black')
-    save_btn.place(x=240, y=590)
+    save_btn = tk.Button(root,text = 'Save Updates', command = lambda: save(name_v), font=('garamond', 12, 'bold'), bg=btcol, fg='black')
+    save_btn.place(x=330, y=680)
 
 
     root.mainloop()
